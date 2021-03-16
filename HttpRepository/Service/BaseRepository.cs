3ec9f17c19631ffe5b92ca8f4822bdfd611bf3e6
Abhirup Sadhukhan <abhirupsadhukhan@gmail.com>
2021-03-16 16:23:58 +0530

    cleaned

--- HttpRepository/Service/BaseRepository.cs
+++ HttpRepository/Service/BaseRepository.cs
@@ -1,9 +1,6 @@
 ﻿using HttpRepository._Contract;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
